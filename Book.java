bfc8917a3e625469a09854deb3fabeb9b7f45961
mayank1365 <mayank1365@gmail.com>
2025-08-19 23:20:59 +0530

    Book class

---
+++ Book.java
@@ -0,0 +1,75 @@
+public class Book {
+    String title;
+    String author;
+    double userRating;
+    int reviews;
+    double price;
+    int year;
+    String genre;
+
+    public Book(String title, String author, double userRating, int reviews, double price, int year, String genre) {
+        this.title = title;
+        this.author = author;
+        this.userRating = userRating;
+        this.reviews = reviews;
+        this.price = price;
+        this.year = year;
+        this.genre = genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(double userRating) {
+        this.userRating = userRating;
+    }
+
+    public int getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(int reviews) {
+        this.reviews = reviews;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
